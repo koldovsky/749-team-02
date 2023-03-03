4e62e161bfbb631990798124edd8fadeae3c1247
luvthenika <124041223+luvthenika@users.noreply.github.com>
2023-03-03 12:43:32 +0000

    footer

--- footer.js
+++ footer.js
@@ -1,31 +1,40 @@
-(function(){
-    const form=document.querySelector('.footer__contact-form');
-    const submit=document.querySelector('.submit__btn');
-    
-    submit.addEventListener('click',handleSubmit);
-    async function handleSubmit(event){
-            event.preventDefault()
-            const status=document.querySelector('.status__submit')
-            const response= await fetch(event.target.action,{
-                method:form.method,
-                body:new FormData,
-                headers:{
-                Accept:"appliaction/json"},
-            })
-            if(response.ok){
-                status.innerHTML="submition is successfully completed";
-                FormData.reset();
-            }
-            else{
-                const data=response.json
-                if(Object.hasOwn(data,"errors")){
-                    status.innerHTML = data["errors"].map((error) => error["message"]).join(", ");
-        }
-                else{
-                    status.innerHTML="Oops! There was a problem submitting your form"
-                }
-            }
-        }
+(function () {
+    const form = document.querySelector(".footer__contact-form");
+    form.addEventListener("submit", handleSubmit);
+  
+    function handleSubmit(event) {
+      event.preventDefault();
+      const status = document.getElementById(".status__submit");
+      fetch(event.target.action, {
+        method: form.method,
+        body: new FormData(event.target),
+        headers: {
+          Accept: "application/json",
+        },
+      })
+        .then((response) => {
+          if (response.ok) {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset();
+          } else {
+            response.json().then((data) => {
+              if (Object.hasOwn(data, "errors")) {
+                status.innerHTML = data["errors"]
+                  .map((error) => error["message"])
+                  .join(", ");
+              } else {
+                status.innerHTML =
+                  "Oops! There was a problem submitting your form";
+              }
+            });
+          }
+        })
+        .catch((error) => {
+          status.innerHTML = "Oops! There was a problem submitting your form";
+        });
+    }
+  
+  })();
         
     //form validation//
         const regexText = /^[a-zA-Z ]{3,}$/
@@ -37,23 +46,22 @@
             const nameValue=nameInput.value;
             if (regexText.test(nameValue)) {
                 nameLabel.innerHTML='Valid name!';
-                nameLabel.style.color="green"
-                nameInput.style.border='1px solid green'
+                nameLabel.style.color="#c7a17a"
+                nameInput.style.border='1px solid #c7a17a '
               } 
             else if (nameValue.length<=2){
                 nameLabel.innerHTML='Name should contain at least 3 characters'
                 nameLabel.style.color="white";
-                nameInput.style.border='1px solid red';
+                nameInput.style.border='1px solid #513b25';
             }
                 
                 else{
                 nameLabel.innerHTML='Invalid name!';
-                nameLabel.style.color="red";
-                nameInput.style.border='1px solid red';
+                nameLabel.style.color="#c7a17a";
+                nameInput.style.border='1px solid c7a17a';
               }
               
             });
 
+        
 
-
-})();
