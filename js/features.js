39d715f18a79006bddc5350bc9c793e731c0a0d1
ArtemHolikov <102384883+ArtemHolikov@users.noreply.github.com>
2023-03-07 12:16:29 +0000

    maked lightbox

--- js/features.js
+++ js/features.js
@@ -1,22 +1,31 @@
 (function () {
     const photos = document.querySelectorAll('.photos img');
-    let currentImageIndex = 0;
     const images = [];
+    let currentImageIndex = 0;
+    let prevButton, nextButton, lightboxImg;
 
     photos.forEach((photo) => {
+        const img = document.createElement('img');
+        img.src = photo.src;
+        img.alt = photo.alt;
+        images.push(img);
+
+
         photo.addEventListener('click', () => {
             const lightbox = document.createElement('div');
             lightbox.classList.add('lightbox');
-
+    
             const img = document.createElement('img');
             img.src = photo.src;
             img.alt = photo.alt;
-
+    
             images.push(img);
-
-            const prevButton = document.createElement('button');
+    
+            lightbox.appendChild(img);
+            
+            prevButton = document.createElement('button');
             prevButton.classList.add('prev-button');
-            prevButton.innerHTML = '&#8592;';
+            prevButton.innerHTML = '&lt;';
             prevButton.addEventListener('click', () => {
                 currentImageIndex--;
                 if (currentImageIndex < 0) {
@@ -24,46 +33,37 @@
                 }
                 updateLightboxImage();
             });
-
-            const nextButton = document.createElement('button');
+            lightbox.appendChild(prevButton);
+    
+            nextButton = document.createElement('button');
             nextButton.classList.add('next-button');
-            nextButton.innerHTML = '&#8594;';
-            nextButton.addEventListener('click', () => {
+            nextButton.innerHTML = '&gt;';
+            nextButton.addEventListener('click', (e) => {
+                e.stopPropagation();
                 currentImageIndex++;
                 if (currentImageIndex >= images.length) {
                     currentImageIndex = 0;
                 }
                 updateLightboxImage();
             });
-
+            lightbox.appendChild(nextButton);
+    
+            function updateLightboxImage() {
+                const currentImage = images[currentImageIndex];
+                img.src = currentImage.src;
+                img.alt = currentImage.alt;
+            }
+    
             const closeButton = document.createElement('button');
             closeButton.classList.add('close-button');
             closeButton.innerHTML = '&times;';
             closeButton.addEventListener('click', () => {
                 lightbox.remove();
             });
-
-            lightbox.appendChild(img);
-            lightbox.appendChild(prevButton);
-            lightbox.appendChild(nextButton);
             lightbox.appendChild(closeButton);
-
+    
             lightbox.classList.add('visible');
             document.body.appendChild(lightbox);
-
-            lightbox.addEventListener('click', () => {
-                lightbox.remove();
-            });
-
-            currentImageIndex = images.length - 1;
         });
     });
-
-    function updateLightboxImage() {
-        const lightboxImg = document.querySelector('.lightbox img');
-        const currentImage = images[currentImageIndex];
-        lightboxImg.src = currentImage.src;
-        lightboxImg.alt = currentImage.alt;
-    }
-
 })();
